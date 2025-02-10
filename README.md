f0bf1bfb801f87a422edc8977a2242454c8d64d9
Surya Pratap Singh <surajsamar06@gmail.com>
2025-02-10 23:14:35 +0530

    Updated README, app.js, index.html and added package.json

---
+++ README.md
@@ -0,0 +1,20 @@
+# Namaste React
+
+# Parcel
+-Dev Build
+-Local Server
+-HMR Hot Module Replacement
+-File Watching Algorithm - written in C++
+-Caching - Faster Builds
+-Image Optimization
+-Minification
+-Bundling
+-Compress
+-Consistency hashing
+-code splitting
+-Differntial Bundling - support older browser
+-Diagnostic
+-Error Handling
+-HTTPs
+-Tree Shaking - remove unused code
+-Different dev and prod bundles
--- app.js
+++ app.js
@@ -1,6 +1,5 @@
-        //const handling = React.createElement("h1",{id : "heading"},"Hello World From React !");
-        //console.log(handling);
-        //<div id = parent
+import React from "react";
+import ReactDOM from "react-dom/client";
 
         const parent = React.createElement("div",
             {id : "parent"},[React.createElement("div",
@@ -9,7 +8,5 @@
                     React.createElement("div",
                         {id : "child2"},[React.createElement("h1"
                             ,{},"H1 tag is here"),React.createElement("h2",{},"H2 tag is here")])]);
-        console.log(parent);
         const root = ReactDOM.createRoot(document.getElementById("root"));
-        console.log(root);
         root.render(parent);
