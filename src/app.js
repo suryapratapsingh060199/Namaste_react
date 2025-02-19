9fe8cfdddad5125044fa1c4d174d06f74b1732e5
Surya Pratap Singh <surajsamar06@gmail.com>
2025-02-20 00:35:47 +0530

    Updated README, app.js, index.html and added package.json

--- src/app.js
+++ src/app.js
@@ -3,6 +3,9 @@ import ReactDOM from "react-dom/client";
 import Header from "./Components/Header";
 import Body from "./Components/Body";
 import Footer from "./Components/Footer";
+import { createBrowserRouter,RouterProvider } from "react-router-dom";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
 
 const FoodApp = () => (
     <div className="app">
@@ -11,6 +14,21 @@ const FoodApp = () => (
         <Footer/>
     </div>
 );
+const router = createBrowserRouter([
+    {
+        path : "/",
+        element : <FoodApp/>,
+    },
+    {
+        path : "/about",
+        element : <About/>,
+    },
+    {
+        path : "/contact",
+        element : <Contact/>,
+    }
+
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<FoodApp/>);
+root.render(<RouterProvider router={router}/>);
